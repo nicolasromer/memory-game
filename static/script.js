014ffde640271adbca599c3e90ca080f201b6fc7
nicolasromer <nicolasromer@gmail.com>
2021-01-20 17:07:20 +0100

    finish game steps

--- static/script.js
+++ static/script.js
@@ -2,6 +2,7 @@ console.log('script loaded');
 
 // static configuration
 const gameSizes = [4,8,12];
+const cardHeight = 200;
 
 // dom references
 const instructionDiv = document.getElementById('instruction');
@@ -9,7 +10,15 @@ const gameDiv = document.getElementById('game');
 const buttonDiv = document.getElementById('button');
 
 // dom helpers
-const getCardHtml = (number, handleClick) => `<div class="card" data-number="${number}">${number}</div>`;
+const getCardHtml = (number) => (
+    // ToDo: make accessible with keyboard
+    `<div
+        class="card"
+        data-number="${number}"
+    >
+        ${number}
+    </div>`
+);
 const getCards = () => Array.from(document.getElementsByClassName('card'));
 const getCardNumber = (card) => parseInt(card.dataset.number, 10);
 const insertCards = (numbers) => {
@@ -19,6 +28,21 @@ const hideCard = card => {
     card.style.height = '10px';
     card.style.color = 'rgb(0, 0, 0, 0)';
     setTimeout(() => card.remove(), 200);
+}
+const hideCards = () => getCards().forEach(card => hideCard(card));
+const flipDown = (card) => {
+    card.style.height = '0px';
+    card.style.color = 'rgb(0, 0, 0, 0)';
+    setTimeout(()=>{
+        card.style.height = cardHeight +'px';
+    }, 200);
+}
+const flipUp = (card) => {
+    card.style.height = '0px';
+    card.style.color = 'black';
+    setTimeout(()=>{
+        card.style.height = cardHeight +'px';
+    }, 200);
 }
 const setInstruction = instruction => {
     instructionDiv.innerHTML = `<p>${instruction}</p>`;
@@ -41,22 +65,96 @@ const showGameSizeChoice = (state, nextGameStep) => {
 const memorizeCards = (state, nextGameStep) => {
     setInstruction('Okay now, memorize these cards!');
 
-    buttonDiv.innerHTML = "<button>I'm Ready</button>";
-    buttonDiv.onclick = nextGameStep(state);
-
     const cardCount = state.gameSize;
+
     const getRandomBelow100 = () => Math.floor(Math.random() * Math.floor(100))
+
+    // fixme: possible to have two numbers the same
     const numbers = [...new Array(cardCount)].map(() => getRandomBelow100())
+
     insertCards(numbers);
+
+    const newState = {...state, cardNumbers: numbers};
+
+    buttonDiv.innerHTML = "<button>I'm Ready</button>";
+    buttonDiv.onclick = () => {
+        buttonDiv.innerHTML = '';
+        nextGameStep(newState);
+    }
 }
 
+const isLowest = (number, set) => number === Math.min(...set);
+const remove = (number, set) => set.splice(set.indexOf(number), 1)
+
+const guessingPhase = (state, nextGameStep) => {
+    setInstruction('Now, click the cards in order, from lowest to highest.');
+
+    let hiddenCards = [...state.cardNumbers];
+
+    let score = 0
+
+    const endRound = () => {
+        hideCards();
+        nextGameStep({...state, score});
+    }
+
+    getCards().forEach((card, index) => {
+        // rolling thunder
+        setTimeout(flipDown(card), index * 50);
+
+        card.addEventListener("click", e => {
+            const number = getCardNumber(card);
+            if (isLowest(number, hiddenCards)) {
+                flipUp(card);
+                // fixme: mutation
+                remove(number, hiddenCards);
+                score += 1;
+            }
+
+            if (!hiddenCards.length) {
+                endRound()
+            }
+        })
+    });
+
+    buttonDiv.innerHTML = "<button>I Give Up :(</button>";
+    buttonDiv.onclick = () => {
+        endRound()
+    }
+}
+
+const scoringPhase = (state, nextGameStep) => {
+    setInstruction('Your score is:');
+
+    const {score, gameSize} = state;
+
+    const perfect = score === gameSize;
+
+    gameDiv.innerHTML = `
+        <div id="final-score" class="${perfect ? 'perfect': ''}">
+            ${score}/${gameSize}
+        </div>
+        <h1>${perfect ? 'Perfect Score! Beer\'s on me.' : 'Nice Try!'}</h1>
+    `;
+
+    buttonDiv.innerHTML = "<button>Again, again!</button>";
+    buttonDiv.onclick = () => {
+        nextGameStep();
+    }
+}
+
+// TODO: use a generator function here;
+// FIXME: this will overflow the stack after enough games
 const startGame = () => {
     showGameSizeChoice({},
-        (state) => memorizeCards(state, ()=> {}),
+        (state2) => memorizeCards(state2,
+            (state3)=> guessingPhase(state3,
+                (state4) => scoringPhase(state4,
+                        startGame
+                    )
+                )
+        )
     );
-
-
-
 }
 
 
